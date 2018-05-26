2c360b43cbc69c0ed44c18c88383b25c4b056c30
Wokarol <wojtek.karolczak20@gmail.com>
2018-05-26 23:25:06 +0200

    Added base scene and scene for scene loading testing

---
+++ Assets/MenuFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunctions : MonoBehaviour {
+
+	[SerializeField] GameObject loadingPage;
+
+	private void Start () {
+		if (loadingPage != null) {
+			loadingPage.SetActive(false);
+		}
+	}
+
+	public void LoadScene (string name) {
+		if(loadingPage != null) {
+			loadingPage.SetActive(true);
+		}
+		SceneManager.LoadScene(name);
+	}
+
+}
